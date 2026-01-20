0d4b3cac384dc6389bab436e8203c65747c92583
Aishwary <aishwary0212@gmail.com>
2026-01-20 19:46:49 +0530

    mongodb

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 const PORT=process.env.PORT || 5000;
-mongoose.connect(process.env.MONGO_URI).then(() => console.log("DB Connected"));
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("DB Connected")).catch((err) => console.log(err));
 
 const UserSchema = new mongoose.Schema({
   name: String,
